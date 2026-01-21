636f24d4581af7797a51a43a7fff31424bec5d2f
QudratullaevAbduazim <abduazimqudratullaev@gmail.com>
2026-01-21 13:11:36 +0500

    full

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path("login/", views.LoginView.as_view()),
+    path("register/", views.RegisterView.as_view()),
+    path("users/", views.UserListAPIView.as_view()),
 ]
     
